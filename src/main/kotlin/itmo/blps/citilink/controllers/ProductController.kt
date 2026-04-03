c1b2399b4780a64d30b4bc10f5bd6b2bb8584314
soniina <sonikpav@gmail.com>
2026-04-03 14:41:30 +0300

    refactor: convert ProductController to json api

--- src/main/kotlin/itmo/blps/citilink/controllers/ProductController.kt
+++ src/main/kotlin/itmo/blps/citilink/controllers/ProductController.kt
@@ -1,36 +1,28 @@
 package itmo.blps.citilink.controllers
 
+import itmo.blps.citilink.dto.responses.ProductResponse
+import itmo.blps.citilink.dto.responses.toResponse
 import itmo.blps.citilink.services.CartService
 import itmo.blps.citilink.services.ProductService
 import itmo.blps.citilink.services.UserService
-import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.CookieValue
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
-@RequestMapping("/")
+@RestController
+@RequestMapping("/products")
 class ProductController(private val productService: ProductService, private val userService: UserService, private val cartService: CartService) {
 
-    @GetMapping
-    fun listProductsOfDay(
-        @CookieValue(value = "session_id", required = false) sessionId: String?,
-        model: Model
-    ): String {
-        val user = sessionId?.let { userService.getUser(it) }
+    @GetMapping("/of-the-day")
+    fun getProductsOfDay(): ResponseEntity<List<ProductResponse>> {
+        val products = productService.getProductsOfDay().map { it.toResponse() }
 
-        if (user == null) {
-            model.addAttribute("cartItemsCount", 0)
-            model.addAttribute("cartProductIds", emptyList<Long>())
-        } else {
-            val cartItems = cartService.getCartItems(user)
-            model.addAttribute("cartItemsCount", cartItems.sumOf { it.quantity })
-            model.addAttribute("cartProductIds", cartItems.map { it.product.id })
+        if (products.isEmpty()) {
+            return ResponseEntity.noContent().build()
         }
 
-        model.addAttribute("productsOfDay", productService.getProductsOfDay())
-
-        return "index"
+        return ResponseEntity.ok(products)
     }
+
 }
