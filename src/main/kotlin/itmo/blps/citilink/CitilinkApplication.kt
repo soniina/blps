92d6b0a2edd31cc1f1822939d2f0938540cc9b0a
soniina <sonikpav@gmail.com>
2026-04-05 20:45:10 +0300

    refactor: integrate Spring JTA with WildFly and configure JNDI DataSource

--- src/main/kotlin/itmo/blps/citilink/CitilinkApplication.kt
+++ src/main/kotlin/itmo/blps/citilink/CitilinkApplication.kt
@@ -1,22 +1,18 @@
 package itmo.blps.citilink
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class CitilinkApplication
+class CitilinkApplication : SpringBootServletInitializer() {
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        return builder.sources(CitilinkApplication::class.java)
+    }
+}
 
 fun main(args: Array<String>) {
     System.setProperty("java.security.auth.login.config", "src/main/resources/jaas.config")
     runApplication<CitilinkApplication>(*args)
 }
-//fun main(args: Array<String>) {
-//    val encoder = org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder()
-//    println("GENERATING HASHES")
-//    println("Hash for 'password123': ${encoder.encode("password123")}")
-//    println("Hash for 'admin_pass': ${encoder.encode("admin_pass")}")
-//    println("END GENERATING")
-//
-//    System.setProperty("java.security.auth.login.config", "src/main/resources/jaas.config")
-//    runApplication<CitilinkApplication>(*args)
-//}
